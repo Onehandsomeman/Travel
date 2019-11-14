41b9ce6c97a4d4a12c8fb97fb5ea4e4f5f3d4109
Onehandsomeman <834429870@qq.com>
2019-11-14 09:19:12 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+import fastClick from 'fastclick'
+import router from './router'
+import './assets/styles/reset.css'
+import '@/assets/styles/iconfont.css'
+
+fastClick.attach(document.body)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632</span>
+      输入城市/游玩/景点</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+.header
+  display flex
+  line-height .86rem
+  background: $bgColor
+  color #fff
+  .header-left
+    width: .64rem
+    text-align center
+    .back-icon
+      font-size .4rem
+  .header-right
+    width: 1.24rem
+    text-align center
+    .arrow-icon
+      font-size .24rem
+  .header-input
+    flex 1
+    background: #fff
+    border-radius .1rem
+    margin-top .12rem
+    line-height .64rem
+    color #ccc
+    height: .64rem
+    margin-left .2rem
+    padding-left .2rem
+</style>
